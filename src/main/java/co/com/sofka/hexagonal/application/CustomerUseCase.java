c063d407aa3f38bf4912d43ab2b0b11ab923a0d2
Diego Gonzalez <diego.gonzalez@sofka.com.co>
2022-12-23 10:09:04 -0300

    Creación de casos de uso

--- src/main/java/co/com/sofka/hexagonal/application/CustomerUseCase.java
+++ src/main/java/co/com/sofka/hexagonal/application/CustomerUseCase.java
@@ -1,4 +1,36 @@
 package co.com.sofka.hexagonal.application;
 
-public class CustomerUseCase {
+import co.com.sofka.hexagonal.domain.Customer;
+import co.com.sofka.hexagonal.infraestructure.inputport.CustomerInputPort;
+import co.com.sofka.hexagonal.infraestructure.outputport.EntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CustomerUseCase implements CustomerInputPort {
+
+    @Autowired
+    EntityRepository entityRepository;
+
+    @Override
+    public Customer createCustomer(String name, String country){
+        Customer customer = Customer.builder()
+                .id(UUID.randomUUID().toString())
+                .name(name)
+                .country(country)
+                .build();
+        return entityRepository.save(customer);
+    }
+
+    @Override
+    public Customer getById(String customerId){
+        return entityRepository.getById(customerId, Customer.class);
+    }
+
+    @Override
+    public List<Customer> getAll(){
+        return entityRepository.getAll(Customer.class);
+    }
+
 }
---
+++ src/main/java/co/com/sofka/hexagonal/application/OrderUseCase.java
@@ -0,0 +1,26 @@
+package co.com.sofka.hexagonal.application;
+
+import co.com.sofka.hexagonal.domain.Order;
+import co.com.sofka.hexagonal.infraestructure.inputport.OrderInputPort;
+import co.com.sofka.hexagonal.infraestructure.outputport.EntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+public class OrderUseCase implements OrderInputPort {
+    @Autowired
+    EntityRepository entityRepository;
+
+    @Override
+    public Order createOrder(String customerId, BigDecimal total){
+        Order order = Order.builder()
+                .id(UUID.randomUUID().toString())
+                .customerId(customerId)
+                .total(total)
+                .build();
+
+        return entityRepository.save(order);
+    }
+}
